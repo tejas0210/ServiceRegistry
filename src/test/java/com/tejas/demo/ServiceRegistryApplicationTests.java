bc3e150a04bd4917e770fde7ae42589406089633
Tejas <tejaskatke900a@gmail.com>
2023-12-27 00:47:39 +0530

    Initial commit

---
+++ src/test/java/com/tejas/demo/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
